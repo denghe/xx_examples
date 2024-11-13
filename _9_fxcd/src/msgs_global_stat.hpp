4d01963ac634fc8c92d860ef5a8e2d92d5607d0b
denghe <denghe@hotmail.com>
2024-11-13 11:10:13 +0800

    minor

--- _9_fxcd/src/msgs_global_stat.hpp
+++ _9_fxcd/src/msgs_global_stat.hpp
@@ -7,7 +7,7 @@ namespace Msgs {
 			memset(this, 0, sizeof(StatPanelBase));
 		}
 
-		XX_INLINE inline Stat_t& StatPanelBase::At(StatTypes t) const {
+		XX_INLINE Stat_t& StatPanelBase::At(StatTypes t) const {
 			assert((uint32_t)t <= (uint32_t)StatTypes::__POINTS_END__);
 			return ((Stat_t*)this)[(uint32_t)t];
 		}
@@ -16,7 +16,7 @@ namespace Msgs {
 			memset(this, 0, sizeof(StatPanel));
 		}
 
-		XX_INLINE inline Stat_t& StatPanel::At(StatTypes t) const {
+		XX_INLINE Stat_t& StatPanel::At(StatTypes t) const {
 			assert((uint32_t)t <= (uint32_t)StatTypes::__RESULTS_END__);
 			return ((Stat_t*)this)[(uint32_t)t];
 		}
@@ -25,7 +25,7 @@ namespace Msgs {
 			memset(this, 0, sizeof(StatPanelExt));
 		}
 
-		XX_INLINE inline Stat_t& StatPanelExt::At(StatTypes t) const {
+		XX_INLINE Stat_t& StatPanelExt::At(StatTypes t) const {
 			assert((uint32_t)t <= (uint32_t)StatTypes::__MAX_VALUE__);
 			return ((Stat_t*)this)[(uint32_t)t];
 		}
