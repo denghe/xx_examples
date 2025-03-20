65d66c45ff6fc6351ad13c75838ac54c22e7cbaa
denghe <denghe@hotmail.com>
2025-03-20 18:54:12 +0800

    add color hit check example

--- _b_platform/src/scene_map.h
+++ _b_platform/src/scene_map.h
@@ -7,6 +7,16 @@ namespace Map {
 		xx::Map map;
 		XYi beginPos{}, endPos{};
 
+		static constexpr float cAnimFPS{ 60.f };
+		static constexpr int32_t cNumAnimFrames{ 60 };
+		static constexpr float cAnimFrameDelay{ 1.f * cAnimFPS / Cfg::fps };
+
+		XY framePos{}, frameScale{ 1,1 }, frameAnchor{ 0.5, 0 };
+		XYi frameSize{};
+		xx::Listi32<xx::Ref<xx::Frame>> frames;
+		xx::Listi32<std::unique_ptr<xx::RGBA8[]>> frameColorss;
+		float frameIndex{};
+
 		void Init() override;
 		void Update() override;
 		void Draw() override;
--- _b_platform/src/scene_map.hpp
+++ _b_platform/src/scene_map.hpp
@@ -45,13 +45,83 @@ namespace Map {
 			xx::CoutN();
 		}
 
+		// load frames & frame colors
+		for (int32_t i = 0; i < cNumAnimFrames; ++i) {
+			auto fn = xx::ToStringFormat("pics/b_redstar_idle_{0}.png", i);
+			auto& f = frames.Emplace(gLooper.LoadFrame(fn));
+			auto width = f->tex->Width();
+			auto height = f->tex->Height();
+			auto bytesLen = width * height * 4;
+			fn = xx::ToStringFormat("pics/c_redstar_idle_{0}.png", i);
+			auto [fileData, fullPath] = gLooper.LoadFileData(fn);
+			auto data = std::make_unique_for_overwrite<xx::RGBA8[]>(bytesLen);
+			{
+				int w, h, comp;
+				if (auto buf = stbi_load_from_memory((stbi_uc*)fileData.buf, (int)fileData.len, &w, &h, &comp, 0)) {
+					xx_assert(width == w);
+					xx_assert(height == h);
+					xx_assert(comp == 4);	// RGBA
+					memcpy(data.get(), buf, bytesLen);
+					stbi_image_free(buf);
+				}
+				else {
+					xx::CoutN("failed to load texture. fn = ", fullPath);
+					xx_assert(false);
+				}
+			}
+			frameColorss.Emplace(std::move(data));
+
+			if (i) {
+				xx_assert(width == frameSize.x);
+				xx_assert(height == frameSize.y);
+			}
+			else {
+				frameSize.x = width;
+				frameSize.y = height;
+			}
+		}
 	}
 
 	inline void Scene::Update() {
+		frameIndex += cAnimFrameDelay;
+		while (frameIndex >= cNumAnimFrames) {
+			frameIndex -= cNumAnimFrames;
+		}
+
+		if (gLooper.mouse.PressedMBLeft() && !gLooper.mouseEventHandler) {
+			auto mp = gLooper.camera.ToLogicPos(gLooper.mouse.pos);
+
+			// todo: calc scale
+			// todo: calculate pos in frame area & print color
+			auto anchorPos = frameSize * XY{ frameAnchor.x, 1.f - frameAnchor.y };
+			xx::FromTo<XY> aabb;
+			aabb.from = framePos - anchorPos;
+			aabb.to = aabb.from + frameSize;
+			
+			if (mp.x < aabb.from.x || mp.y < aabb.from.y
+				|| mp.x >= aabb.to.x || mp.y >= aabb.to.y) {
+				// out of range
+				xx::CoutN("out of range");
+			}
+			else {
+				auto localPos = (mp - aabb.from).As<int32_t>();
+				assert(localPos.x >= 0 && localPos.x < frameSize.x);
+				assert(localPos.y >= 0 && localPos.y < frameSize.y);
+				auto frameColors = frameColorss[(int32_t)frameIndex].get();
+				auto color = frameColors[localPos.y * frameSize.x + localPos.x];
+				xx::CoutN("localPos = ", localPos, " color = ", color);
+			}
+		}
 	}
 
 	inline void Scene::Draw() {
-		gLooper.ctcDefault.Draw({ 0, gLooper.windowSize_2.y - 5 }, "(map) show console", xx::RGBA8_Green, { 0.5f, 1 });
+		xx::Quad().SetFrame(frames[(int32_t)frameIndex])
+			.SetPosition(framePos)
+			.SetAnchor(frameAnchor)
+			.SetScale(frameScale)
+			.Draw();
+
+		gLooper.ctcDefault.Draw({ 0, gLooper.windowSize_2.y - 5 }, "(map) show console. mouse left button for hit check", xx::RGBA8_Green, { 0.5f, 1 });
 	}
 
 }
